9ce45231d1bf2d964491b81652bc500ff28ad29b
Ryan Ramsey <rramsey1115@gmail.com>
2024-08-11 12:27:35 -0500

    login added

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -13,3 +13,20 @@ export const getAllUseres = () => {
 export const getUserById = (id) => {
     return fetch(`http://localhost:8088/users?id=${id}`).then(res => res.json());
 }
+
+export const getUserByEmail = (email) => {
+    return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
+      res.json()
+    )
+  }
+  
+  export const createUser = (customer) => {
+    return fetch("http://localhost:8088/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(customer),
+    }).then((res) => res.json())
+  }
+  
